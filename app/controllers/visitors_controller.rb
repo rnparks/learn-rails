6ded5cb01150dc181623409dc075483073dfab72
Ryan Parks <rnparks@gmail.com>
2016-04-07 11:59:12 -0400

    working zurb

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,9 +2,6 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
-    #flash[:notice] = "Welcome!"
-    #flash[:alert] = "My birthday is coming soon."
-    render 'visitors/new'
   end
 
 end
